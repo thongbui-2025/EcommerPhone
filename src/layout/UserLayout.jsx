4ba67688fc8564f5d4d09d709ba91c1463b32630
vanthong <buivanthong1704@gmail.com>
2025-02-17 13:17:08 +0700

    Footer commit

--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const UserLayout = () => {
 	return (
 		<div>
 			<Header />
 			<Outlet />
+			<Footer />
 		</div>
 	);
 };
